a2b09c2556999ff6d743ba0a23b677611188eb9e
Jason Smylnycky <jason@peachworks.com>
2016-03-19 14:38:06 -0400

    Updated so all front end code is under /client

---
+++ client/app/components/hero-detail/hero-detail.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+
+import { Hero } from '../../interfaces/hero';
+import { HeroService } from '../../services/hero/hero';
+
+@Component({
+  selector: 'my-hero-detail',
+  moduleId: 'components/hero-detail/hero-detail',
+  templateUrl: 'hero-detail.html',
+  styleUrls: ['hero-detail.css']
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(
+    private _heroService: HeroService,
+    private _routeParams: RouteParams) {
+  }
+
+  ngOnInit() {
+    let id = +this._routeParams.get('id');
+    this._heroService.getHero(id)
+      .then(hero => this.hero = hero);
+  }
+
+  goBack() {
+    window.history.back();
+  }
+}
