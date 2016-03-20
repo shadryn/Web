a4c4dd260b4ee1cd8b134be2cbefae3d1a93f857
Jason Smylnycky <jason@peachworks.com>
2016-03-20 11:07:14 -0400

    Fixed up dev process

---
+++ client/app/services/progression/progression.ts
@@ -0,0 +1,15 @@
+import {Injectable} from 'angular2/core';
+import {Boss} from '../../interfaces/boss';
+import {BOSSES} from './mock-progression';
+@Injectable()
+export class ProgressionService {
+  getBosses() {
+    return Promise.resolve(BOSSES);
+  }
+
+  getBoss(id: number) {
+    return Promise.resolve(BOSSES).then(
+      bosses => bosses.filter(boss => boss.id === id)[0]
+    );
+  }
+}
