e9fa0bdae0cddbe0f81398d16309b67dfabf5804
jlsmylnycky <jsmylnycky@digitalroots.com>
2016-03-25 12:42:24 -0400

    added the js for the bear but jason will have to refactor to make it nice.

--- client/app/components/the-guild/the-guild.ts
+++ client/app/components/the-guild/the-guild.ts
@@ -17,3 +17,20 @@ export class TheGuildComponent implements OnInit {
     console.log(sectionKey);
   }
 }
+$(function () {
+    var list_slideshow = $("#bear-sayings"),
+        listItems = list_slideshow.children('li'),
+        listLen = listItems.length,
+        i = 0,
+        randomNum = function() {
+        	return Math.floor(Math.random() * listLen);
+        },
+        changeList = function () {
+            listItems.eq(i).fadeOut(300, function () {
+                i = randomNum();
+                listItems.eq(i).fadeIn(300);
+            });
+        };
+    listItems.not(':first').hide();
+    setInterval(changeList, 5000);
+});
