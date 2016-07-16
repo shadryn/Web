d7c120aa03c14fc7a96d7e88bf70ac144d1235ef
Jason <jason784@gmail.com>
2016-07-16 10:51:10 -0400

    member photos

--- app/routes/raiding.js
+++ app/routes/raiding.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-import fetch from 'ember-network/fetch';
-import { API_PATH, API_MEMBERS_PATH } from '../const';
+//import fetch from 'ember-network/fetch';
+//import { API_PATH, API_MEMBERS_PATH } from '../const';
 
 export default Ember.Route.extend({
 
