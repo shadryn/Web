e7eca61d5494bfa6a094c0a0efeeab631e5e903c
Jason <jason784@gmail.com>
2016-07-15 21:20:58 -0400

    Populate characters

---
+++ app/const.js
@@ -0,0 +1,2 @@
+export const API_PATH = 'https://api.nobreaksguild.com/v1';
+export const API_MEMBERS_PATH = '/guild/members';
--- app/routes/raiding.js
+++ app/routes/raiding.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
+import fetch from 'ember-network/fetch';
+import { API_PATH, API_MEMBERS_PATH } from '../const';
 
 export default Ember.Route.extend({
+
+  model() {
+    return fetch(API_PATH + API_MEMBERS_PATH)
+      .then((response) => { return response.json(); }); /*
+      .then((members) => {
+
+        let transformed = [];
+        members.forEach((member) => {
+          transformed.push({
+            id: member._id,
+            type: 'member',
+            attributes: {
+              name: member.name
+            },
+            relationships: []
+          });
+        });
+
+        console.dir(transformed);
+
+        this.get('store').push({data: transformed});
+
+        console.dir(this.get('store').findAll('member'));
+      }); */
+  }
 });
