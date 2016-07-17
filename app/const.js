da6d4b9fbae1b1c2d86063749ff0cc5cc86ca5ce
Jason <jason784@gmail.com>
2016-07-17 12:14:23 -0400

    fix raiders endpoint

--- app/const.js
+++ app/const.js
@@ -1,3 +1,3 @@
 export const API_PATH = 'https://api.nobreaksguild.com/v1';
-export const API_MEMBERS_PATH = '/guild/members';
+export const API_MEMBERS_PATH = '/guild/No%20Breaks/raiders';
 export const API_EPGP_UPLOAD_PATH = '/guild/No%20Breaks/epgp/upload';
