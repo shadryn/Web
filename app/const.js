e60117ef5b814876dd6f57b2616caf851a52df2a
Jason <jason784@gmail.com>
2016-07-16 20:05:27 -0400

    consts

--- app/const.js
+++ app/const.js
@@ -1,3 +1,3 @@
-export const API_PATH = 'http://localhost:5001/v1'; //; 'https://api.nobreaksguild.com/v1';
+export const API_PATH = 'https://api.nobreaksguild.com/v1';
 export const API_MEMBERS_PATH = '/guild/members';
-export const API_EPGP_UPLOAD_PATH = '/guild/No%20Breaks/epgp/upload'
+export const API_EPGP_UPLOAD_PATH = '/guild/No%20Breaks/epgp/upload';
