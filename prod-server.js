3e98b2b6764142395875794ec79d40c3fad7fd90
Jason <jason784@gmail.com>
2016-07-19 19:53:30 -0400

    fix to other routes

--- prod-server.js
+++ prod-server.js
@@ -5,6 +5,8 @@ let express = require('express');
 
 var app = express();
 app.use('/', express.static(__dirname + '/dist'));
+app.use('*', express.static(__dirname + '/dist'));
+
 
 //app.get('/*', fastbootMiddleware(express.static(__dirname + '/dist')));
 
