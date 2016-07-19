3d0c3388f73072ae3cf5f03d7e2196aeb9afe27d
Jason <jason784@gmail.com>
2016-07-19 19:41:24 -0400

    Remove angular garbage

--- client/app/vendor.ts
+++
@@ -1,8 +0,0 @@
-// Polyfills
-import 'es6-shim';
-import 'es6-promise';
-import 'zone.js';
-import 'reflect-metadata';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import './styles.scss';
