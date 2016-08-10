4a9a0952fef2266be691c59625d3e3832f59a033
Jason <jason784@gmail.com>
2016-08-10 18:12:17 -0400

    Remove garbage

--- app/const.js
+++ app/const.js
@@ -1,6 +1,7 @@
 export const API_PATH = 'https://api.nobreaksguild.com/v1';
+export const CDN_PATH = 'https://api.nobreaksguild.com/cdn';
 export const API_RAIDERS_PATH = '/guild/No%20Breaks/epgp/raiders';
 export const API_EPGP_LOOT_PATH = '/guild/No%20Breaks/epgp/loot';
 export const API_EPGP_UPLOAD_PATH = '/guild/No%20Breaks/epgp/upload';
 
-export const CLASS_MAP = ['','Warrior','Paladin','Hunter','Rogue','Priest','DeathKnight','Shaman','Mage','Warlock','Monk','Druid'];
+export const CLASS_MAP = ['','Warrior','Paladin','Hunter','Rogue','Priest','DeathKnight','Shaman','Mage','Warlock','Monk','Druid','DemonHunter'];
